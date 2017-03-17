cef8df9cfb7ac5a61ae80d63bc820fbb9fd81f02
AaronFaltesek <aaron.faltesek@gmail.com>
2017-03-16 19:28:37 -0500

    additional commenting and clean up

--- Question4.R
+++ Question4.R
@@ -1,7 +1,8 @@
 #Question 4: Plot the GDP for all of the countries. Use ggplot2 
 #to color your plot by Income Group.
 
-#below I use the ggplot to 
+#below I use the ggplot to display the comparison of GDP and income group. I also include colors in the color option. I use geom_jitter because it is useful for comparying a continuous 
+#variable against a categorical one. I also use ggtitle to title and change axis names
 ggplot(sortedMerge, aes(Income.Group,V5 )) +
   geom_jitter(data = NULL, color=as.integer(factor(sortedMerge$Income.Group)), size = 3) +
   ggtitle("GDP vs Income Groups") +
--- Question5.R
+++ Question5.R
@@ -1,6 +1,8 @@
 #Question 5:Cut the GDP ranking into 5 separate quantile groups. Make a table versus Income.Group.
 #How many countries are Lower middle income but among the 38 nations with highest
 #GDP?
+
+#The below is uesd to divide the data up into quantiles. 
 sortedMergeQuantTest <- within(sortedMerge, quantile <- as.integer(cut(V5, quantile(V5, probs=0:5/5), include.lowest=TRUE)))
 
 #now that i have the quantiles i want to make a table of quant vs income
@@ -8,6 +10,7 @@ testTable <- table(sortedMergeQuantTest$quantile,sortedMergeQuantTest$Income.Gro
 
 #now I need to determine how many countries are in the top 38 countries by GDP and in the lower middle income group
 #this would be cell row 5, column 4. 
+#I use a cleanup table to remove an unnessesary columnn
 cleanUpTable <- testTable[,2:6]
 print(cleanUpTable)
 print(paste(cleanUpTable[5,4],"countries are in the top 38(top quantile based on GDP) and also in the lower middle income group category"))
